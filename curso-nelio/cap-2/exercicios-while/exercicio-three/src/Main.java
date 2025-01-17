ddd6c96f7a63b1ab3b6ccc1dad76831a02344517
Daniel San <danielgdt88@hotmail.com>
2025-01-17 13:12:41 -0300

    feat: exercício três while

---
+++ curso-nelio/cap-2/exercicios-while/exercicio-three/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+//TIP Para <b>executar</b> o código, pressione <shortcut actionId="Run"/> ou
+// clique no ícone <icon src="AllIcons.Actions.Execute"/> no gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int alcool = 0;
+        int gasolina = 0;
+        int diesel = 0;
+        int code = sc.nextInt();
+
+
+        while (code != 4) {
+
+            if (code == 0) code = sc.nextInt();
+            if(code == 1) alcool++;
+            if(code == 2) gasolina++;
+            if(code == 3) diesel++;
+
+            code = sc.nextInt();
+        }
+        System.out.println("MUITO OBRIGADO");
+        System.out.println("Alcool: " + alcool);
+        System.out.println("Gasolina: " + gasolina);
+        System.out.println("Diesel: " + diesel);
+
+        sc.close();
+    }
+}
