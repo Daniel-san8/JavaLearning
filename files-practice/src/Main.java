3f1ff4c67b5721f6023c05c513645a1b16d75607
Daniel San <danielgdt88@hotmail.com>
2025-01-29 21:18:31 -0300

    feat: finalizdo

--- files-practice/src/Main.java
+++ files-practice/src/Main.java
@@ -1,23 +1,29 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
         String path = "summary.csv";
         String[] vectLines;
+        List<String> listString;
+
 
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line = br.readLine();
-
-            while (line != null) {
+            String line;
+            BufferedWriter newFile = new BufferedWriter(new FileWriter("asummary.csv", true));
+            while ((line = br.readLine()) != null) {
+                listString = new ArrayList<>();
                 vectLines = line.split(",");
-                System.out.println(Arrays.toString(vectLines));
-                line = br.readLine();
+                double valueProduct = Double.parseDouble(vectLines[1]) * Double.parseDouble(vectLines[2]);
+                vectLines[1] = String.valueOf(valueProduct);
+                listString.add(vectLines[0]);
+                listString.add(vectLines[1]);
+                newFile.write(String.join(",", listString));
+                newFile.newLine();
             }
+            newFile.flush();
         } catch (IOException err) {
             err.printStackTrace();
         }
