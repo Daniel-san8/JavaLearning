fe61ed02122272fa2a27cb35bd94c3f9abc271b9
Daniel San <danielgdt88@hotmail.com>
2025-02-03 21:02:41 -0300

    feat: crio classe e interface

---
+++ exercicio-interface/src/entities/CalculationParcelas.java
@@ -0,0 +1,5 @@
+package entities;
+
+public interface CalculationParcelas {
+    public double calcular(double salario);
+}
---
+++ exercicio-interface/src/entities/Contract.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class Contract {
+
+    Integer parcelas;
+
+
+    public Contract () {
+
+    }
+
+    public Contract (int parcelas) {
+        this.parcelas = parcelas;
+    }
+}
